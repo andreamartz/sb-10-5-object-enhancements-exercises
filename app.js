048da4b37ac6a56ca029b2051e4d1c5d7a2d18a5
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-14 14:50:04 -0500

    Write createAnimal fcn

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var instructor = {
   },
 };
 
-console.log(instructor);
+// console.log(instructor);
 
 // Object Methods ES2015 (my task)
 const instructor3 = {
@@ -63,4 +63,35 @@ const instructor3 = {
   },
 };
 
-console.log(instructor3);
+// console.log(instructor3);
+
+// **********************************************************
+// createAnimal function (GIVEN)
+// Write a function which generates an animal object.
+// The function should accept 3 arguments:
+
+// * species: the species of animal (‘cat’, ‘dog’)
+// * verb: a string used to name a function (‘bark’, ‘bleet’)
+// * noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+
+// Use one or more of the object enhancements we’ve covered.
+
+//const d = createAnimal("dog", "bark", "Woooof!");
+// {species: "dog", bark: ƒ}
+//d.bark(); //"Woooof!"
+
+//const s = createAnimal("sheep", "bleet", "BAAAAaaaa");
+// {species: "sheep", bleet: ƒ}
+//s.bleet(); //"BAAAAaaaa"
+
+const createAnimal = (species, verb, noise) => {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+};
+
+// console.log("createAnimal: ", createAnimal("sheep", "bleet", "BAAAAaaaa"));
+// console.log(s.bleet());
